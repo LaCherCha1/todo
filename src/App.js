fbe300e86f77e8177f147efde75f2e73be2b3f66
Franklin Gonzalez <franklinomarflores@gmail.com>
2020-04-06 18:09:57 -0400

    Style Ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { render } from 'react-dom';
+export default class App extends React.Component {
+  
 
-function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+  render(){
+    return (
+      <div className="container">
+        <div className='header'>
+          <h1>My Todo list</h1>
+          <input 
+          className='text-input' 
+          type='text' 
+          placeholder='New Todo'
+           />
+          <button className='btn'>Save</button>
+        </div>
+        <div className='body'>
+          <ul className='list'>
+            <li>
+              <label>
+                <input className='checkbox' type='checkbox' />
+                <span className="checkmark"></span>
+                todo
+              </label>
+              <FontAwesomeIcon className='del' icon={ faTrashAlt } />
+            </li>
+            <li>
+              <label>
+                <input className='checkbox' type='checkbox' />
+                <span className="checkmark"></span>
+                todo 2
+              </label>
+              <FontAwesomeIcon className='del' icon={ faTrashAlt } />
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
 }
 
-export default App;
+
